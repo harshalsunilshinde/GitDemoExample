79b8d1452b866901f9b6eb083b2ea16b515c89fa
Harshal <harshalshinde085@gmail.com>
2022-07-27 10:51:58 +0530

    creating project

---
+++ src/main/java/com/acks/GitDemoExampleApplication.java
@@ -0,0 +1,13 @@
+package com.acks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoExampleApplication.class, args);
+	}
+
+}
